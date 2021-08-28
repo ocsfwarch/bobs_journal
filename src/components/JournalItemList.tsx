494a1ccff781ee4bc01085d247f437c19075425c
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-08-28 08:49:58 -0700

    Add statement to Landing Page in the event there are no existing journal entries.

--- src/components/JournalItemList.tsx
+++ src/components/JournalItemList.tsx
@@ -4,6 +4,7 @@
 */
 
 import JournalItem from "./JournalItem";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 
 interface IProps {
   journal: {
@@ -16,49 +17,61 @@ interface IProps {
 
 const JournalItemList: React.FC<IProps> = (props: IProps) => {
   const renderList = (): JSX.Element[] => {
-    // Check if we need to sort the list
-    if (props.journal && props.journal.length > 1) {
-      // Sort the top level entries
-      props.journal.sort((a: any, b: any) => {
-        let aDate = new Date(a.date);
-        let bDate = new Date(b.date);
-        return bDate.getTime() - aDate.getTime();
-      });
-      // Sort the journal entries
-      for (let item of props.journal) {
-        if (item.entries && item.entries.length > 1) {
-          item.entries.sort((a: any, b: any) => {
-            let aDate = new Date(`${item.date}T${a.time}`);
-            let bDate = new Date(`${item.date}T${b.time}`);
-            return bDate.getTime() - aDate.getTime();
-          });
+    if (props.journal && props.journal.length >= 1) {
+      // Check if we need to sort the list
+      if (props.journal && props.journal.length > 1) {
+        // Sort the top level entries
+        props.journal.sort((a: any, b: any) => {
+          let aDate = new Date(a.date);
+          let bDate = new Date(b.date);
+          return bDate.getTime() - aDate.getTime();
+        });
+        // Sort the journal entries
+        for (let item of props.journal) {
+          if (item.entries && item.entries.length > 1) {
+            item.entries.sort((a: any, b: any) => {
+              let aDate = new Date(`${item.date}T${a.time}`);
+              let bDate = new Date(`${item.date}T${b.time}`);
+              return bDate.getTime() - aDate.getTime();
+            });
+          }
         }
       }
+      return props.journal.map((entry) => {
+        return (
+          <li className="li-journal" key={entry.date}>
+            <section>
+              <header>{entry.formatdate}</header>
+              <div className="">
+                <ul className="ul-journal-entries">
+                  {entry.entries.map((item, index) => {
+                    return (
+                      <JournalItem
+                        key={`${entry.date}-${index}`}
+                        entryDate={entry.date}
+                        index={index}
+                        item={item}
+                        removeFromJournal={props.removeFromJournal}
+                      />
+                    );
+                  })}
+                </ul>
+              </div>
+            </section>
+          </li>
+        );
+      });
+    } else {
+      return [
+        <li className="li-journal-noentries" key="none">
+          <span>There are no entries available</span>
+          <p>
+            Please select the <AddCircleIcon />
+            at the top right of the application to add journal entries.
+          </p>
+        </li>,
+      ];
     }
-    return props.journal.map((entry) => {
-      return (
-        <li className="li-journal" key={entry.date}>
-          <section>
-            <header>{entry.formatdate}</header>
-            <div className="">
-              <ul className="ul-journal-entries">
-                {entry.entries.map((item, index) => {
-                  return (
-                    <JournalItem
-                      key={`${entry.date}-${index}`}
-                      entryDate={entry.date}
-                      index={index}
-                      item={item}
-                      removeFromJournal={props.removeFromJournal}
-                    />
-                  );
-                })}
-              </ul>
-            </div>
-          </section>
-        </li>
-      );
-    });
   };
   return (
     <section className="journal-container">
